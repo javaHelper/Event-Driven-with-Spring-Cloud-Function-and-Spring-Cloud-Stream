c1cc9fbd0ca33ae7591c7f103ad0c303eea90f79
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-09 20:27:00 +0530

    Web Functions

---
+++ 3.web-function/src/test/java/com/example/demo/WebTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class WebTests {
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	void testFunctionDefinitionApi() {
+		webClient.post()
+				.contentType(MediaType.TEXT_PLAIN)
+				.bodyValue("Spring Cloud")
+				.exchange()
+				.expectBody(String.class)
+				.isEqualTo("DUOLC GNIRPS");
+	}
+}
